d670c219602a1964e0680f6f635e936c1d38d623
vnkama <220-230@mail.ru>
2021-07-30 17:47:26 +0400

    to_home

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import numpy as np
 from numba import cuda          # Библиотека Nvidia для работы с GPU
+import numba as nb
 import time
 
-
-def init():
+#
+#
+#
+def init_const_arrays():
     A = np.array(
             [
                 [
@@ -35,6 +38,21 @@ def init():
             ],
             dtype=np.double
     )
+
+
+    # результат перемножния первой пары матриц
+    #
+    #   [[-72.1, 221, -389.7],
+    #   [244.2, -562.5, 841.2],
+    #   [-383.7,	-576, 327.9]]
+
+
+    # результат перемножния второй пары матриц
+    #  [[-12.2	-1210	-1200],
+    # [-67.2	-8250	-9780],
+    # [89	9910	10920]]
+
+
     return A, B
 
 
@@ -42,9 +60,13 @@ def init_1d(size):
     return np.zeros(shape=size)
 
 ########################################################
+########################################################
+
+
 
 @cuda.jit
 def test_1d_1block__cuda_jit(A):
+
     # thread index in block
     tx = cuda.threadIdx.x
 
@@ -72,34 +94,38 @@ def test_1d_1block__cuda_jit(A):
 # максимальный разме 1-мерного блока = 1024 thread's
 #
 def test_1d_1block():
-    array_size = 1000000
+    array_size = 100000
+    loop_count = 100
     A = np.zeros(shape=array_size)
 
-    t1 = time.perf_counter()
 
-    #d_A = cuda.to_device(A)
+    t1 = time.perf_counter()
+    d_A = cuda.to_device(A)
 
     if array_size <= 256:
         blocks_per_grid = 1
         threads_per_block = array_size
     else:
         threads_per_block = 256
-        blocks_per_grid = ((array_size-1) // threads_per_block)+1
+        blocks_per_grid = ((array_size-1) // threads_per_block) + 1
 
     print(f'blocks_per_grid: {blocks_per_grid}, threads_per_block: {threads_per_block}')
 
-    for _ in range(10000):
-        test_1d_1block__cuda_jit[blocks_per_grid, threads_per_block](A)
+    for _ in range(loop_count):
+        test_1d_1block__cuda_jit[blocks_per_grid, threads_per_block](d_A)
 
 
-    #A = d_A.copy_to_host()
+    A = d_A.copy_to_host()
+
     t2 = time.perf_counter()
-    print(t2-t1)
+    print((t2-t1)/loop_count/array_size*1e6)
 
     print('sum A:', np.sum(A))
 
+
 ########################################################
 ########################################################
+
 
 @cuda.jit
 def test_1d_Nblock__cuda_jit(A):
@@ -149,71 +175,103 @@ def test_1d_Nblock():
 
     t2 = time.perf_counter()
 
-
     print(t2-t1)
     print('sum A:', np.sum(A) / loop_count)
-
 
 ########################################################
 ########################################################
 
 @cuda.jit
-def test_2d_1block__cuda_jit(A):
+def matmul_f1__cuda_jit(A, B, C):
+
+    tpb = (4, 4)
+    bpg = cuda.gridDim.x
+
     # thread index in block
     tx = cuda.threadIdx.x
+    ty = cuda.threadIdx.y
+    #x, y = cuda.grid(2)
 
-    # block index in grid
-    ty = cuda.blockIdx.x
+
+    # индекс матрицы 3x3
+    bx = cuda.blockIdx.x
     array_size = A.shape[0]
 
 
-    # Block width, i.e. number of threads per block
-    threads_per_block = cuda.blockDim.x
-    #print('tx:', tx, 'ty:', ty, 'threads_per_block:', threads_per_block)
+    if tx >= 3 or ty >= 3:
+        return
+
+    sA = cuda.shared.array(shape=tpb, dtype=nb.float32)
+    sB = cuda.shared.array(shape=tpb, dtype=nb.float32)
+    sA[tx, ty] = A[bx, tx, ty]
+    sB[tx, ty] = B[bx, tx, ty]
+    C[bx, tx, ty] = sA[tx, 0] * sB[0, ty] + sA[tx, 1] * sB[1, ty] + sA[tx, 2] * sB[2, ty]
+
+    # A_mx33 = A[bx]
+    # B_mx33 = B[bx]
+    # C[bx, tx, ty] = A_mx33[tx, 0] * B_mx33[0, ty] + A_mx33[tx, 1] * B_mx33[1, ty] + A_mx33[tx, 2] * B_mx33[2, ty]
 
 
-    # Compute flattened index inside the array
-    i = tx + ty * threads_per_block
 
-    if i < array_size:  # Check array boundaries
-        A[i] += 1
 
 #
-# входной массив: 2d,
-# в сетке - 1 блок
+# тестируем умножение группы матриц
+# матрицы 3x3, их несколько, потому входной массив (N,3,3)
+# в этой функции 1 матрица 3x3 соответствует 1 блоку
+#
+# в сетке - N блоков размером минимум 3x3, но по факту делаем 4x4 или
 # максимальный размер 2-мерного блока = 1024  x 1024 thread's
 #
-def test_2d_1block__cuda_jit():
-    array_size = 100000
-    A  = init_1d(array_size)
+def test_matmul_f1():
+    array_size = 10000
+    loop_count = 1000
 
-    if array_size <= 256:
-        blocks_per_grid = 1
-        threads_per_block = array_size
-    else:
-        threads_per_block = 1024
-        blocks_per_grid = ((array_size-1) // threads_per_block)+1
+    # 1000 10000, 0.026мкс на операцию
 
-    print(f'blocks_per_grid: {blocks_per_grid}, threads_per_block: {threads_per_block}')
+    A = np.random.uniform(low=-100, high=100, size=(array_size, 3, 3))
+    B = np.random.uniform(low=-100, high=100, size=(array_size, 3, 3))
+    A = np.float32(A)
+    B = np.float32(B)
 
-    loop_count = 10000
+
+    #A, B = init_const_arrays()
+
+    C = np.zeros_like(A)
+    #array_size = A.shape[0]
 
     t1 = time.perf_counter()
 
+    d_A = cuda.to_device(A)
+    d_B = cuda.to_device(B)
+    d_C = cuda.to_device(C)
+
+    blocks_per_grid = array_size
+    threads_per_block = (4, 4)
+
+    #print(f'blocks_per_grid: {blocks_per_grid}, threads_per_block: {threads_per_block}')
+
+
     for _ in range(loop_count):
-        test_1d_Nblock__cuda_jit[blocks_per_grid, threads_per_block](A)
+        matmul_f1__cuda_jit[blocks_per_grid, threads_per_block](d_A, d_B, d_C)
+
+    A = d_A.copy_to_host()
+    B = d_B.copy_to_host()
+    C = d_C.copy_to_host()
 
     t2 = time.perf_counter()
 
-
     print(t2-t1)
-    print('sum A:', np.sum(A) / loop_count)
+    print('мкс:', (t2-t1) / loop_count / array_size * 1e6)
+
+    # print('A:\n', A)
+    # print('B:\n', B)
+    # print('C:\n', C)
 
 ########################################################
 ########################################################
 
 def main():
-    test_1d_1block()
+    test_matmul_f1()
 
 
 
